1021c6dd5751c9f44abb78ab1b716db0d51cd6d6
sss-soham <soham.sakunde@gmail.com>
2025-03-08 23:24:42 +0530

    ES - module

---
+++ node/esModule/app.js
@@ -0,0 +1,10 @@
+//const math = require("./math");
+
+import mult from "./math.js"
+
+// console.log(math.add(5,10));
+// console.log(math.subs(10,5));
+console.log(mult(50,10));
+// console.log(math.div(10,5));
+// console.log(math.PI);
+// console.log(PIPI);
---
+++ node/esModule/math.js
@@ -0,0 +1,24 @@
+const add = (a,b) => {
+    return a+b;
+};
+
+const subs = (a, b) => {
+    return a-b;
+};
+
+const mult = (a,b) => {
+    return a*b;
+};
+
+const div = (a, b) => {
+    return a/b;
+};
+
+const PI = 3.14;
+
+// module.exports.add = add;
+// module.exports.subs = subs;
+
+// module.exports = {add, subs, mult, div, PI};
+
+export default mult;
